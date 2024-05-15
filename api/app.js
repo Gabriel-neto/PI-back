5da9594d427d81dfe4cfac23d0ddd26fd074df8c
Guilherme França <souzagfranca@gmail.com>
2024-05-15 11:08:26 -0300

    Implementação do banco de dados para o endpoint /produtos #3

--- api/app.js
+++ api/app.js
@@ -1,10 +1,14 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+const mongoose = require("mongoose");
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
 var app = express();
 
-app.use(logger('dev'));
+mongoose.connect(process.env.MONGODB_URL);
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
