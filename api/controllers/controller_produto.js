148ca5c550d6e441e1b3d58047747bdbd1a4d345
Guilherme França <souzagfranca@gmail.com>
2024-05-20 15:31:54 -0300

    Implementado o endpoint para criar um novo produto #10

--- api/controllers/controller_produto.js
+++ api/controllers/controller_produto.js
@@ -1,5 +1,9 @@
 const Produto = require("../models/model_produto");
 const mongoose = require("mongoose");
 
+async function criar(req, res) {
+  const produto = await Produto.create(req.body);
+  res.status(201).json(produto);
+}
 
 module.exports = { criar };
