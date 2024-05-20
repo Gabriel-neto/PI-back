535e3dacf5212d986bf1f6c9b6b70ffdd74c55d7
Eduardo Fernandes <iamedusm@gmail.com>
2024-05-20 14:52:59 -0300

    Criação da rota para retornar uma lista de fornecedores: Close #38

---
+++ api/tests/router_fornecedor.test.js
@@ -0,0 +1,20 @@
+const supertest = require('supertest');
+
+const app = require('../app');
+
+const request = supertest(app);
+
+let id = null
+
+describe('API CRUD de Fornecedores', () => {
+    
+    /*
+    * Rotas GET
+    */
+    test("Deve retornar 200 e um array no GET /fornecedor", async() => {
+        const response = await request.get("/fornecedor")
+        expect(response.status).toBe(200)
+        expect(response.type).toBe("application/json")
+        id = response.body.length > 0 ? response.body[0]._id.toString() : null
+    })
+});
