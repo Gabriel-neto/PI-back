4b4af36224a86f292f1a1688896ba07bcabc91d7
Eduardo Fernandes <iamedusm@gmail.com>
2024-05-20 14:42:35 -0300

    Criaçaõ do arquivo de configuração: Close #37

--- api/app.js
+++ api/app.js
@@ -1,8 +1,12 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var app = express();
+
+mongoose.connect(process.env.MONGODB_URL)
 
 app.use(logger('dev'));
 app.use(express.json());
