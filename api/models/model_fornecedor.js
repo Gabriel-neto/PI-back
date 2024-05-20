ab2a24c782ee01f4c2d673c0b040edbf18f64670
Eduardo Fernandes <iamedusm@gmail.com>
2024-05-20 14:39:50 -0300

    criação da model_forncedor: Close #32

---
+++ api/models/model_fornecedor.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    nome: {
+        type: String,
+        trim: true,
+        uppercase: false,
+        required: true
+    },
+    empresa: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    cnpj: {
+        type: String,
+        required: true,
+        trim: true,
+        match: /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/ // Regex para validar formato do CNPJ
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        match: /^\S+@\S+\.\S+$/ // Regex simples para validar formato de e-mail
+    },
+    telefone: {
+        type: String,
+        trim: true,
+        match: /^\(\d{2}\) \d{4,5}-\d{4}$/
+    },
+    dataCadastro: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Fornecedor', schema);
