a5b9bb31af3ee1963acdf89b1fe683e4f2ac3e45
Eduardo Fernandes <iamedusm@gmail.com>
2024-05-20 17:16:41 -0300

    Criação do arquivo de rotas /api-docs: Close #43

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const routerApidocs = require('./routes/router_apidocs');
 const routerFornecedor = require("./routes/router_fornecedor")
 
 var app = express();
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use('/api-docs', routerApidocs);
 app.use('/fornecedor', routerFornecedor)
 
 module.exports = app;
