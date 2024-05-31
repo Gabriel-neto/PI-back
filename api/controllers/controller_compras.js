f2932703ee9f149c90a4b72f79c39622456a4042
Pedro Lima <pedrolima38@hotmail.com>
2024-05-31 19:05:18 -0300

    Criação do controlador de compras, closes #52

---
+++ api/controllers/controller_compras.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const Compra = require('../models/model_compras')
+
+const criar = async (req, res) => {
+  const compra = await Compra.create(req.body)
+  res.status(201).json(compra)
+}
+
+const obterTodas = async (req, res) => {
+  const compras = await Compra.find({});
+  res.json(compras);
+};
+
+const buscarPeloId = async (req, res, next) => {
+  try {
+      const id = new mongoose.Types.ObjectId(req.params.id)
+      const compra = await Compra.findOne({ _id: id });
+      if (compra) {
+          next();
+      } else {
+          res.status(404).json({ msg: 'Compra não encontrada!' })
+      }
+  } catch (error) {
+      res.status(400).json({ msg: 'id inválido!' });
+  }
+}
+
+const obter = async (req, res) => {
+  const id = new mongoose.Types.ObjectId(req.params.id);
+  const compra = await Compra.findOne({ _id: id });
+  res.status(200).json(compra);
+};
+
+const atualizar = async (req, res) => {
+  const id = new mongoose.Types.ObjectId(req.params.id);
+  const compra = await Compra.findByIdAndUpdate({_id: id}, req.body);
+  res.status(200).json(compra);
+};
+
+const remover = async (req,res) => {
+  const id = new mongoose.Types.ObjectId(req.params.id);
+  await Compra.findByIdAndDelete({_id: id});
+  res.status(204).end();
+};
+
+
+
+module.exports = { criar, obterTodas, obter, buscarPeloId, atualizar, remover };
