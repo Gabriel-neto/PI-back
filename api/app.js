7d8b2036ea1e217dada18abe1c143f0a9614a054
Pedro Lima <pedrolima38@hotmail.com>
2024-05-31 19:33:52 -0300

    Criação do arquivo app.js e implementação de todos os endpoint, closes #78 #56 #60 #63 #66 #72

--- api/app.js
+++ api/app.js
@@ -1,12 +1,22 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const routerApiDocs = require('./routes/router_apidocs');
+const routerCompras = require('./routes/router_compras');
+
 var app = express();
+
+mongoose.connect(process.env.MONGODB_URL);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use('/api-docs', routerApiDocs);
+app.use('/compras', routerCompras);
 
 module.exports = app;
