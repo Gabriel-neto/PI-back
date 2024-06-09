db4626abcd6b66e52c9f90547a0915c06d4e3c72
Pedro Lima <pedrolima38@hotmail.com>
2024-06-09 17:48:20 -0300

    Teste do GET retornando uma lista de compras e o status 200

--- api/tests/router_compras.test.js
+++ api/tests/router_compras.test.js
@@ -6,17 +6,12 @@ const request = supertest(app);
 let id = null
 
 describe('API BlackBox', () => {
-    test('Deve retornar 201 e um JSON no POST /compras', async () =>{
-        const response = await request.post('/compras').send({ 
-            produto: 'CAMISA',
-            preco: 60.0,
-            quantidade: 10,
-            data: "2024-05-31",
-            vendedor: 'VENDEDOR X',
-            nf: 123456
-        });
-        expect(response.status).toBe(201);
+   test('Deve retornar 200 e um array no GET /compras', async () => {
+        const response = await request.get('/compras');
+        expect(response.status).toBe(200);
         expect(response.type).toBe('application/json');
-        id = response.body._id;
-    });
+        if (response.body.length > 0) {
+            id = response.body[0]._id.toString();
+        }
+    });  
 });
