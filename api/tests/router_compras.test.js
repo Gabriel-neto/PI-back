e032299f2bd20a97f18d9cddda92a7065667281e
Pedro Lima <pedrolima38@hotmail.com>
2024-06-09 17:56:00 -0300

    Teste do GET status 404, close #80

--- api/tests/router_compras.test.js
+++ api/tests/router_compras.test.js
@@ -7,7 +7,7 @@ let id = null
 
 describe('API BlackBox', () => {
     test('Deve retornar 200 e um JSON no GET /compras/id', async () => {
-        const response = await request.get(`/compras/665a63b7680c5a780f106f4d`);
+        const response = await request.get(`/compras/665a63b7680c5a780f106f70`);
         expect(response.status).toBe(200);
         expect(response.type).toBe('application/json')
     });
