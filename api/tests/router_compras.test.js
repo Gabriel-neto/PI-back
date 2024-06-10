14f75fcd0e0e57bf9bae513c5ba2080bcdbc65ca
Pedro Lima <pedrolima38@hotmail.com>
2024-06-10 15:51:07 -0300

    Teste do DELETE  retornando o status 204, close #73

--- api/tests/router_compras.test.js
+++ api/tests/router_compras.test.js
@@ -6,9 +6,9 @@ const request = supertest(app);
 let id = null
 
 describe('API BlackBox', () => {
-    test('Deve retornar 422 e um JSON no POST /compras', async () => {
-        const response = await request.post('/compras').send({});
-        expect(response.status).toBe(422);
-        expect(response.type).toBe("application/json");
+    test('Deve retornar 204 no DELETE /compras/id', async () => {
+        const response = await request.delete(`/compras/665a6326967438467b4423cf`);
+        expect(response.status).toBe(204);
+        expect(response.type).toBe('');
     });
 });
