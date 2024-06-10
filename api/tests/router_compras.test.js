5ed6ce7039232dc39ba35ed63be333b9e766319e
Pedro Lima <pedrolima38@hotmail.com>
2024-06-10 16:01:39 -0300

    Teste do DELETE  retornando o status 404, close #74

--- api/tests/router_compras.test.js
+++ api/tests/router_compras.test.js
@@ -6,9 +6,9 @@ const request = supertest(app);
 let id = null
 
 describe('API BlackBox', () => {
-    test('Deve retornar 204 no DELETE /compras/id', async () => {
-        const response = await request.delete(`/compras/665a6326967438467b4423cf`);
-        expect(response.status).toBe(204);
-        expect(response.type).toBe('');
+    test('Deve retornar 404 e um JSON no DELETE /compras/id', async () => {
+        const response = await request.delete(`/compras/665a632637fc04dca9991670`);
+        expect(response.status).toBe(404);
+        expect(response.type).toBe('application/json');
     });
 });
